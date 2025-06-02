8d303beec1fc7523d083e67ad4dcf636e7808545
pchalupa <chalupa.petr93@gmail.com>
2025-06-02 12:13:00 +0200

    chore: update release flow

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,10 @@ import { type Options, defineConfig } from "tsup";
 
 export default defineConfig((options: Options) => ({
 	entryPoints: ["src/index.ts"],
+	outDir: ".",
 	format: "esm",
 	target: "es2022",
 	platform: "node",
+	dts: true,
 	...options,
 }));
